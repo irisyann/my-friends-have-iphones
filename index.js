1463a6813b4a5f8672551951ccb90c672e0c40d1
Iris Y <48094822+irisyann@users.noreply.github.com>
2023-07-05 14:58:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const convert = require('heic-convert');
+const { promisify } = require('util');
+
+async function convertHEICtoPNG() {
+    const inputFolderDir = path.join(__dirname, 'heic_photos');
+
+    fs.readdir(inputFolderDir, (err, files) => {
+        files.forEach(async file => {
+            const inputFileDir = path.join(__dirname, 'heic_photos', file);
+            const outputFileDir = path.join(__dirname, 'converted_photos', file.replace('.HEIC', '.png'));
+
+            console.log(inputFileDir)
+
+            const inputBuffer = await promisify(fs.readFile)(inputFileDir);
+            const outputBuffer = await convert({
+                buffer: inputBuffer, 
+                format: 'PNG',     
+                quality: 1           
+            });
+            
+            await promisify(fs.writeFile)(outputFileDir, outputBuffer);
+        });
+    });
+}
+
+convertHEICtoPNG();
